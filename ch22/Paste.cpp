e28499f606eb59d8d0f08e678736b89f9165609f
Zhou Peng <jacob.zhoupeng@gmail.com>
2012-09-16 11:40:00 +0800

    finish chapter 22, start chapter 23: error handling.

--- ch22/Paste.cpp
+++ ch22/Paste.cpp
@@ -13,8 +13,10 @@ class MyBase { // Repair Vendor interface
 public:
     virtual void v() const = 0;
     virtual void f() const = 0;
+
     // New interface function:
     virtual void g() const = 0;
+
     virtual ~MyBase() {
         out << "~MyBase()\n";
     }
@@ -42,22 +44,23 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-    Paste1& p1p = *new Paste1;
+    Paste1& p1p = *new Paste1; //! Óï·¨ºÜÆæ¹Ö
     MyBase& mp = p1p; // Upcast
+
     out << "calling f()\n";
-
     mp.f(); // Right behavior
+    
     out << "calling g() \n";
-
     mp.g(); // New behavior
+
     out << "calling A(p1p)\n";
-
     A(p1p); // Same old behavior
+    
     out << "calling B(p1p)\n";
-
     B(p1p); // Same old behavior
+    
     out << "delete mp\n";
-
+    
     // Deleting a reference to heap object:
     delete &mp; // Right behavior
 
--- ch22/Persist2.cpp
+++ ch22/Persist2.cpp
@@ -39,9 +39,11 @@ class WData1 : public Persistent, public Data {
 public:
     WData1(float f0 = 0.0, float f1 = 0.0, float f2 = 0.0)
         : Data(f0, f1, f2) {}
+    
     void write(ostream& out) const {
         out << f[0] << " " << f[1] << " " << f[2];
     }
+    
     void read(istream& in) {
         in >> f[0] >> f[1] >> f[2];
     }
@@ -51,47 +53,52 @@ class WData2 : public Data, public Persistent {
 public:
     WData2(float f0 = 0.0, float f1 = 0.0, float f2 = 0.0)
         : Data(f0, f1, f2) {}
+    
     void write(ostream& out) const {
         out << f[0] << " " << f[1] << " " << f[2];
     }
+
     void read(istream& in) {
         in >> f[0] >> f[1] >> f[2];
     }
 };
 
-class Conglomerate : public Data, 
-public Persistent {
-    char* name; // Contains a pointer
-    WData1 d1;
-    WData2 d2;
+class Conglomerate : public Data, public Persistent {
 public:
     Conglomerate(const char* nm = "", float f0 = 0.0, float f1 = 0.0,
             float f2 = 0.0, float f3 = 0.0, float f4 = 0.0, 
             float f5 = 0.0, float f6 = 0.0, float f7 = 0.0, 
-            float f8 = 0.0) : Data(f0, f1, f2), d1(f3, f4, f5), 
-            d2(f6, f7, f8) {
+            float f8 = 0.0) 
+                : Data(f0, f1, f2), d1(f3, f4, f5), d2(f6, f7, f8) {
         name = new char[strlen(nm) + 1];
         strcpy(name, nm);
     }
 
     void write(ostream& out) const {
         int i = strlen(name) + 1;
+
         out << i << " "; // Store size of string
         out << name << endl;
+
         d1.write(out);
         d2.write(out);
+
         out << f[0] << " " << f[1] << " " << f[2];
     }
 
     // Must read in reverse order as write:
     void read(istream& in) {
         delete[] name; // Remove old storage
+
         int i;
         in >> i >> ws; // Get int, strip whitespace
+
         name = new char[i];
         in.getline(name, i);
+
         d1.read(in);
         d2.read(in);
+
         in >> f[0] >> f[1] >> f[2];
     }
 
@@ -100,23 +107,32 @@ public:
         d1.print();
         d2.print();
     }
+private:
+    char* name; // Contains a pointer
+    WData1 d1;
+    WData2 d2;
 };
 
 int main(int argc, char* argv[]) {
     {
         ofstream data("data.dat");
         assure(data, "data.dat");
+
         Conglomerate C("This is Conglomerate C", 1.1, 2.2,
             3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9);
         cout << "C before storage" << endl;
+
         C.print();
         C.write(data);
     } // Closes file
 
     ifstream data("data.dat");
     assure(data, "data.dat");
+
     Conglomerate C;
+
     C.read(data);
+
     cout << "after storage: " << endl;
     C.print();
 
