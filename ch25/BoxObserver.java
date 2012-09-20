4ffc254142cf9477c16249bc39d8895a36aa9aa4
Zhou Peng <jacob.zhoupeng@gmail.com>
2012-09-20 23:37:16 +0800

    chapter 25: observer design pattern

---
+++ ch25/BoxObserver.java
@@ -0,0 +1,104 @@
+//: C25:BoxObserver.java
+// Demonstration of Observer pattern using
+// Java's built-in observer classes.
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+// You must inherit a new type of Observable:
+class BoxObservable extends Observable {
+    public void notifyObservers(Object b) {
+        // Otherwise it won't propagate changes:
+        setChanged();
+        super.notifyObservers(b);
+    }
+}
+
+public class BoxObserver extends Frame {
+    Observable notifier = new BoxObservable();
+
+    public BoxObserver(int grid) {
+        setTitle("Demonstrates Observer pattern");
+        setLayout(new GridLayout(grid, grid));
+
+        for (int x = 0; x < grid; x++) {
+            for (int y = 0; y < grid; y++) {
+                add(new OCBox(x, y, notifier));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int grid = 8;
+
+        if (args.length > 0) {
+            grid = Integer.parseInt(args[0]);
+        }
+
+        Frame f = new BoxObserver(grid);
+
+        f.setSize(500, 400);
+        f.setVisible(true);
+        f.addWindowListener(
+            new WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+    }
+}
+
+class OCBox extends Canvas implements Observer {
+    int x; // Location in grid
+    int y;
+
+    Observable notifier;
+    Color cColor = new Color(127, 127, 127);
+
+    static final Color[] colors = {
+        Color.black, Color.blue, Color.cyan,
+        Color.darkGray, Color.gray, Color.green,
+        Color.lightGray, Color.magenta,
+        Color.orange, Color.pink, Color.red,
+        Color.white, Color.yellow
+    };
+
+    static final Color newColor() {
+        return colors[(int)(Math.random() * colors.length)];
+    };
+
+    OCBox(int x, int y, Observable notifier) {
+        this.x = x;
+        this.y = y;
+        notifier.addObserver(this);
+        this.notifier = notifier;
+        addMouseListener(new ML());
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(newColor()); //! 原书有错误，我猜想作者的意图是这样的
+        Dimension s = getSize();
+        g.fillRect(0, 0, s.width, s.height);
+    }
+
+    class ML extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            notifier.notifyObservers(OCBox.this);
+        }
+    }
+
+    public void update(Observable o, Object arg) {
+        OCBox clicked = (OCBox)arg;
+
+        if (nextTo(clicked)) {
+            cColor = clicked.cColor;
+            repaint();
+        }
+    }
+
+    private final boolean nextTo(OCBox b) {
+        return Math.abs(x - b.x) <= 1 
+            && Math.abs(y - b.y) <= 1;
+    }
+} ///:~
---
+++ ch25/designpattern/SingletonPattern.java
@@ -0,0 +1,46 @@
+//: C25:SingletonPattern.java
+// The Singleton design pattern: you can
+// never instantiate more than one.
+package designpattern;
+
+// Since this isn't inherited from a Cloneable
+// base class and cloneability isn't added,
+// making it final prevents cloneability from
+// being added in any derived classes:
+final class Singleton {
+	public static Singleton getHandle() {
+		return s;
+	}
+
+	public int getValue() {
+		return i;
+	}
+
+	public void setValue(int x) {
+		i = x;
+	}
+
+	private static Singleton s = new Singleton(47);
+	private int i;
+
+	//! 防止类在外部实例化
+	private Singleton(int x) {
+		i = x;
+	}
+}
+
+public class SingletonPattern {
+	public static void main(String[] args) {
+		Singleton s = Singleton.getHandle();
+		System.out.println(s.getValue());
+
+		Singleton s2 = Singleton.getHandle();
+		s2.setValue(9);
+		System.out.println(s.getValue());
+
+		try {
+			// Can't do this: compile-time error.
+			// Singleton s3 = (Singleton)s2.clone();
+		} catch (Exception e) {}
+	}
+} ///:~
